2a4f6f388ed6bbc77d07f11baa3c6198dc2601dd
huayu98 <huayu98@vt.edu>
2021-04-20 12:59:54 +0800

    Add

--- app/src/main/java/com/example/remember/MainActivity.java
+++ app/src/main/java/com/example/remember/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
