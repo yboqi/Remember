b6e308ccf1c8415b7fc4cc9cf46f2fa290b89954
huayu98 <huayu98@vt.edu>
2021-04-20 13:04:01 +0800

    Add

--- app/src/main/java/com/example/remember/MainActivity.java
+++ app/src/main/java/com/example/remember/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    Buttom button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
