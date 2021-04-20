31c2e5885b9019ba45e52d0113794994e6d6a443
Hanwen-Cheng <60452962+Hanwen-Cheng@users.noreply.github.com>
2021-04-20 01:47:25 -0400

    12

---
+++ app/src/main/java/com/example/remember/Date.java
@@ -0,0 +1,12 @@
+package com.example.remember;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Date {
+
+    TextView textView;
+    Button button;
+    Button button2;
+
+}
--- app/src/main/java/com/example/remember/MainActivity.java
+++ app/src/main/java/com/example/remember/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.remember;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
